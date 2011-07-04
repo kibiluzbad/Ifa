4149d0eec2c434b0ce947b208d747548fcd7275e
Leonardo Cardoso <kibiluzbad@hotmail.com>
2011-07-04 20:33:32 -0300

    Adding sample app.

---
+++ Ifa.Sample1/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ifa.Model;
+using Ifa.Sample1.Models;
+
+namespace Ifa.Sample1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ICollection<Product> _products;
+        
+        public ProductsController()
+        {
+            InitProducts();
+        }
+
+        private void InitProducts()
+        {
+            _products = new List<Product>();
+            for (int i = 1; i <= 300; i++)
+            {
+                _products.Add(new Product
+                                  {
+                                      Id = i,
+                                      CreatedAt = DateTime.Now,
+                                      Name = "Product" + 1,
+                                      Value = 105.5M*i
+                                  });
+            }
+        }
+
+        //
+        // GET: /Products/
+        public ActionResult Index([DefaultValue(10)]int itemsPerPage,
+            [DefaultValue(1)]int currentPage)
+        {
+            var products = _products.Skip((currentPage - 1)*itemsPerPage).Take(itemsPerPage);
+            var result = new PagedResultViewModel<Product>(itemsPerPage, currentPage, _products.Count, products);
+
+            return View(result);
+        }
+
+    }
+}
---
+++ Ifa.Sample1/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ifa.Sample1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
