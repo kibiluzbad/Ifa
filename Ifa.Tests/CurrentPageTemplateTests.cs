fd76f33d56d2a33b66bc238c3123d415fc82a7d3
Leonardo Cardoso <kibiluzbad@gmail.com>
2011-07-12 11:55:04 -0300

    Test for current page template.

---
+++ Ifa.Tests/CurrentPageTemplateTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ifa.Model;
+using Ifa.Templates;
+using Moq;
+using NUnit.Framework;
+
+namespace Ifa.Tests
+{
+    [TestFixture]
+    public class CurrentPageTemplateTests
+    {
+        [Test]
+        public void Can_Get_HtmlString_For_CurrentPageTemplate()
+        {
+            var tagBuilderMock = new Mock<IHtmlTagBuilder>();
+
+            tagBuilderMock
+                .Setup(c => c.Build(It
+                                        .Is<IDictionary<string, object>>(d => "1" == ""+d["value"]
+                                                                              && "currentPage" == ""+d["class"])))
+                .Returns("<tag class=\"currentPage\">1</tag>")
+                .Verifiable();
+
+            var fakeTagBuilder = tagBuilderMock.Object;
+
+            var currentPage = new CurrentPage(1, 3);
+            var fakeHtmlHelper = new FakeHtmlHelper(currentPage);
+
+            BasicIfaTemplate currentPageTemplate = new CurrentPageTemplate();
+            currentPageTemplate.HtmlTagBuilder = fakeTagBuilder;
+            var htmlString = currentPageTemplate.Get(fakeHtmlHelper);
+
+            Assert.That(htmlString, Is.EqualTo("<tag class=\"currentPage\">1</tag>"));
+            tagBuilderMock.VerifyAll();
+        }
+    }
+}
---
+++ Ifa.Tests/FakeHtmlHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Ifa.Tests
+{
+    public class FakeHtmlHelper : HtmlHelper
+    {
+        public FakeHtmlHelper(ViewContext viewContext, IViewDataContainer viewDataContainer) : base(viewContext, viewDataContainer)
+        {
+        }
+
+        public FakeHtmlHelper(ViewContext viewContext, IViewDataContainer viewDataContainer, RouteCollection routeCollection) : base(viewContext, viewDataContainer, routeCollection)
+        {
+        }
+
+        public FakeHtmlHelper(object model)
+            : base(new FakeViewContext(model), new FakeViewDataContainer())
+        {
+         
+        }
+
+        public class FakeViewContext : ViewContext
+        {
+            public FakeViewContext(object model)
+            {
+                ViewData = new ViewDataDictionary();
+                ViewData.Model = model;
+            }
+        }
+
+        public class FakeViewDataContainer : IViewDataContainer
+        {
+            public ViewDataDictionary ViewData { get; set; }
+
+            public FakeViewDataContainer()
+            {
+                ViewData = new ViewDataDictionary();
+            }
+        }
+    }
+}
